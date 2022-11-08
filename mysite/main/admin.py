2f7f886793d5e2447bb8aedf25ca4b839433d210
Святослав Сухоставский <s.svjatoslav.o@gmail.com>
2022-11-08 20:22:03 +0200

    Добавил модели, настроил админку
    Модели: Product, ProductPhoto, Material, Manufacturer, Category

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,117 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.utils.safestring import mark_safe
+
+from .models import ProductPhoto, Product, Material, Manufacturer, Category
+
+admin.site.site_title = 'Yacht RC'
+admin.site.site_header = 'Админ-панель Yacht RC'
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'discount', 'manufacturer', 'status', 'amount')
+    list_display_links = ('id', 'title')
+    search_fields = ('product_code', 'title', 'slug')
+    fields = (('id', 'product_code'),
+              'slug',
+              ('title', 'price'),
+              'discount',
+              'status',
+              ('average_rating', 'num_of_reviews'),
+              'get_html_photos',
+              ('warranty_period', 'return_period'),
+              'amount',
+              'product_category',
+              'description',
+              'manufacturer',
+              'material',
+              'model_power_supply',
+              'r_controller_power_supply',
+              'engine_type',
+              'channel_frequency',
+              'num_of_channels',
+              'camera_presence',
+              'kit_type',
+              'length',
+              'height',
+              'width',
+              'complete_set',
+              'packing_size',
+              'feature',
+              'date_of_creation')
+
+    prepopulated_fields = {'slug': ('title', 'product_code')}
+    readonly_fields = ('id', 'get_html_photos', 'date_of_creation', 'average_rating', 'num_of_reviews')
+
+    def get_html_photos(self, obj):
+        photos_html = ''
+        for p in ProductPhoto.objects.filter(product=obj.id).order_by('index'):
+            photos_html += f'<a href="/admin/main/productphoto/{p.id}/change/"><img style="margin: 5px"' \
+                           f' src="{p.photo.url}" height=50></a>'
+        return mark_safe(photos_html)
+    get_html_photos.short_description = "Фото"
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'id')
+    fields = ('id', 'name', 'description')
+    readonly_fields = ('id',)
+
+
+admin.site.register(Material, MaterialAdmin)
+
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'slug', 'id')
+    prepopulated_fields = {'slug': ('name',)}
+    fields = ('id', 'name', 'slug', 'get_html_photo', 'photo', 'description')
+    readonly_fields = ('id', 'get_html_photo')
+
+    def get_html_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f"<img src='{obj.photo.url}' height=50>")
+    get_html_photo.short_description = "Фото"
+
+
+admin.site.register(Manufacturer, ManufacturerAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'parent_category')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'slug', 'id')
+    prepopulated_fields = {'slug': ('name',)}
+    fields = ('id', 'name', 'slug', 'get_html_photo', 'photo', 'description', 'parent_category')
+    readonly_fields = ('id', 'get_html_photo')
+
+    def get_html_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f"<img src='{obj.photo.url}' height=50>")
+    get_html_photo.short_description = "Фото"
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductPhotoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'index', 'photo', 'product')
+    list_display_links = ('id', 'photo')
+    search_fields = ('id', 'photo', 'product')
+    fields = ('id', 'get_html_photo', 'photo', 'index', 'product')
+    readonly_fields = ('id', 'get_html_photo')
+
+    def get_html_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f"<img src='{obj.photo.url}' height=50>")
+    get_html_photo.short_description = "Фото"
+
+
+admin.site.register(ProductPhoto, ProductPhotoAdmin)
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,145 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+    ON_SALE = "S"
+    STATUS_TYPES = (
+        ("A", "Архивный товар"),
+        (ON_SALE, "В наличии"),
+        ("E", "Ожидается"),
+    )
+
+    KIT_TYPE = (
+        ("RTR", "(RTR)Ready to Run"),
+        ("RTF", "(RTF)Ready to Fly"),
+        ("PNP", "(PNP)Plug and Play"),
+        ("BND", "(BND)Bind and Drive"),
+        ("BNF", "(BNF)Bind and Fly"),
+        ("KIT", "KIT"),
+        (None, "Не указан"),
+    )
+
+    ENGINE_TYPE = (
+        ("B", "Коллекторный"),
+        ("BL", "Бесколлекторный"),
+        ("ICE", "ДВС"),
+        (None, "Без двигателя"),
+    )
+
+    POWER_SUPPLY = (
+        ("B", "Батарейка"),
+        ("A", "Аккумулятор"),
+        (None, "Не указан"),
+    )
+
+    CHANNEL_FREQUENCY = (
+        ("CF1", "27 МГц"),
+        ("CF2", "40 МГц"),
+        ("CF3", "2.4 ГГц"),
+        (None, "Не указан"),
+    )
+
+    title = models.CharField(max_length=255, verbose_name="Название")
+    price = models.FloatField(verbose_name="Цена")
+    discount = models.FloatField(verbose_name="Скидка", blank=True, null=True)
+    average_rating = models.FloatField(verbose_name="Средний рейтинг", default=0.0)
+    num_of_reviews = models.PositiveSmallIntegerField(verbose_name="Количество отзывов", default=0)
+    product_code = models.PositiveIntegerField(unique=True, verbose_name="Код товара")
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
+    status = models.CharField(max_length=5, choices=STATUS_TYPES, verbose_name="Статус", default=ON_SALE, null=True)
+    warranty_period = models.CharField(max_length=255, verbose_name="Гарантийный срок", blank=True, null=True)
+    return_period = models.CharField(max_length=255, verbose_name="Обмен/возврат", blank=True, null=True)
+    amount = models.PositiveIntegerField(verbose_name="Количество товара в наличии")
+    slug = models.SlugField(max_length=255, unique=True)
+    product_category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name="Категория")
+    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.CASCADE, verbose_name="Производитель")
+    material = models.ManyToManyField('Material', verbose_name="Материал корпуса", blank=True)
+    model_power_supply = models.CharField(max_length=1, choices=POWER_SUPPLY, verbose_name="Источник питания модели",
+                                          null=True, blank=True)
+    r_controller_power_supply = models.CharField(max_length=1, choices=POWER_SUPPLY,
+                                                 verbose_name="Источник питания пульта", null=True, blank=True)
+    engine_type = models.CharField(max_length=3, choices=ENGINE_TYPE, verbose_name="Тип двигателя",
+                                   null=True, blank=True)
+    channel_frequency = models.CharField(max_length=5, choices=CHANNEL_FREQUENCY, verbose_name="Частота",
+                                         null=True, blank=True)
+    num_of_channels = models.PositiveSmallIntegerField(verbose_name="Количество каналов", blank=True, null=True)
+    camera_presence = models.BooleanField(default=False, verbose_name="С камерой")
+    kit_type = models.CharField(max_length=3, choices=KIT_TYPE, verbose_name="Тип комплектации", null=True, blank=True)
+    length = models.PositiveSmallIntegerField(verbose_name="Длина в(мм)", blank=True, null=True)
+    height = models.PositiveSmallIntegerField(verbose_name="Высота в(мм)", blank=True, null=True)
+    width = models.PositiveSmallIntegerField(verbose_name="Ширина в(мм)", blank=True, null=True)
+    complete_set = models.TextField(verbose_name="В комплекте", blank=True, null=True)
+    packing_size = models.CharField(max_length=255, verbose_name="Габариты упаковки", blank=True, null=True)
+    feature = models.TextField(verbose_name="Особенности", blank=True, null=True)
+    date_of_creation = models.DateTimeField(auto_now_add=True, verbose_name="Дата регистрации товара")
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ['average_rating']
+        get_latest_by = "date_of_creation"
+
+    def __str__(self):
+        return self.title
+
+
+class Material(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'материал'
+        verbose_name_plural = 'материалы'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название")
+    photo = models.ImageField(upload_to="photos/manufacturer/%Y/%m/%d/", blank=True, null=True, verbose_name="Фото")
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = 'производитель'
+        verbose_name_plural = 'производители'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    photo = models.ImageField(upload_to="photos/category/%Y/%m/%d/", blank=True, null=True, verbose_name="Фото")
+    description = models.TextField(verbose_name="Описание", blank=True, null=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    parent_category = models.ForeignKey('Category', on_delete=models.CASCADE, blank=True, null=True,
+                                        verbose_name="Родительская категория")
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class ProductPhoto(models.Model):
+    photo = models.ImageField(upload_to="photos/product/%Y/%m/%d/", verbose_name="Фото")
+    index = models.PositiveSmallIntegerField(verbose_name="Номер")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Товар")
+
+    class Meta:
+        verbose_name = 'Фото товара'
+        verbose_name_plural = 'Фото товара'
+        ordering = ['product']
+
+    def __str__(self):
+        return f"id({self.pk}) {self.index} - {self.photo}"
+
