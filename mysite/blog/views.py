9e34c3f3fd67189515706bd552a17ae06c38efb4
Святослав Сухоставский <s.svjatoslav.o@gmail.com>
2022-11-21 12:03:49 +0200

    Базово адаптирована страница блога и страница отдельного поста

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from .models import Post
+
+
 def blog(request):
-    return render(request, "blog/blog.html")
+    all_posts = Post.objects.all()
+    print("ALL_POSTS ", all_posts)
+    context = {'all_posts': all_posts}
+    return render(request, "blog/blog.html", context=context)
 
 
 def post(request, post_slug):
-    return render(request, "blog/blog.html")
+    post_details = Post.objects.get(slug=post_slug)
+    context = {
+        'post': post_details
+    }
+    return render(request, "blog/blog-details.html", context=context)
